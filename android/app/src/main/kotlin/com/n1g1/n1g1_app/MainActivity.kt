9cc3d6155c1b1611a2ae5b17a6f072b0203533ce
Edwin <edwin12sv@gmail.com>
2021-01-25 13:55:35 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/n1g1/n1g1_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.n1g1.n1g1_app
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
